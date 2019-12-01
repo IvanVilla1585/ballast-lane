6f35d99cc60b97b368398a61e04fc40ed9167dfa
Ivan Dario Villa Ramirez <trezeguet55@gmail.com>
2019-12-01 15:16:40 -0500

    contact user controller is implemented

---
+++ api/routes/user-contacts/controller.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const errors = require('restify-errors')
+
+const service = require('./service')
+
+async function create (req, res, next) {
+  try {
+    const result = await service.create(req.body)
+    res.status(201).send(result)
+    next()
+  } catch (err) {
+    next(new errors.InternalServerError('Internal server error'))
+  }
+}
+
+module.exports = {
+  create
+}
