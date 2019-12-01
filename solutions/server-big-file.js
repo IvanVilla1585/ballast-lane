9a3dae17b5571bf63facd4ffeb974b374d824511
Ivan Dario Villa Ramirez <trezeguet55@gmail.com>
2019-12-01 14:50:36 -0500

    linter is ignored

--- solutions/server-big-file.js
+++ solutions/server-big-file.js
@@ -1,5 +1,5 @@
 'use strict'
-
+/* eslint-disable */
 const fs = require('fs')
 const http = require('http')
 
@@ -7,8 +7,8 @@ const server = http.createServer()
 
 function writeFile () {
 	const file = fs.createWriteStream('./big.file')
-	for(let i=0; i<= 1e6; i++) {
-		file.write('Lorem ipsum dolor sit amet, consectetur adispisicing elit, sed, do eiusmod tempor incidiunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n');
+	for(let i = 0; i <= 1e6; i++) {
+		file.write('Lorem ipsum dolor sit amet, consectetur adispisicing elit, sed, do eiusmod tempor incidiunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n')
 	}
 	file.end()
 }
